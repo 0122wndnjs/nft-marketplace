09a440494ea1e7c70ea335598987b90cfa114231
Joowon Kim <0122wndnjs@gmail.com>
2022-01-17 12:26:28 +0900

    Modify contract

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract KryptoBird {
-    string public name;
-    string public symbol;
+import './ERC721Connector.sol';
 
-    constructor() {
-        name = 'KryptoBird';
-        symbol = 'KBIRDZ';
+contract KryptoBird is ERC721Connector {
+    
+    // initialize this contract to inherit name and symbol from erc732metadata
+    // so that the name is KryptoBird and the symbol is KBIRDZ
+    
+    constructor() ERC721Connector('KryptoBird', 'KBIRDZ') {
+        
     }
 }
