af4fa5e669796e28b622ad52ecd682a6326e8ff7
Joowon Kim <0122wndnjs@gmail.com>
2022-01-17 11:31:19 +0900

    first smart contract deployment

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KryptoBird {
+    string public name;
+    string public symbol;
+
+    constructor() {
+        name = 'KryptoBird';
+        symbol = 'KBIRDZ';
+    }
+}
