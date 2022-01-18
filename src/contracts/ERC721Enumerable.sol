b5986ef62852195e4c0236cd7d52dce5be61f489
Joowon Kim <0122wndnjs@gmail.com>
2022-01-18 15:50:53 +0900

    Connect ERC721Enumerable

--- src/contracts/ERC721Enumerable.sol
+++ src/contracts/ERC721Enumerable.sol
@@ -11,33 +11,26 @@ contract ERC721Enumerable is ERC721 {
     mapping(uint256 => uint256) private _allTokensIndex;
     // mapping of owner to list of all owner token ids
     mapping(address => uint256[]) private _ownedTokens;
-    // mapping from token ID index of the owner tokens list
+    // mapping from token ID to index of the owner tokens list
     mapping(uint256 => uint256) private _ownedTokensIndex;
 
-    /// @notice Count NFTs tracked by this contract
-    /// @return A count of valid NFTs tracked by this contract, where each one of
-    ///  them has an assigned and queryable owner not equal to the zero address
     function totalSupply() external view returns (uint256) {
         return _allTokens.length;
     }
 
-    /// @notice Enumerate valid NFTs
-    /// @dev Throws if `_index` >= `totalSupply()`.
-    /// @param _index A counter less than `totalSupply()`
-    /// @return The token identifier for the `_index`th NFT,
-    ///  (sort order not specified)
-    function tokenByIndex(uint256 _index) external view returns (uint256);
+    // function tokenByIndex(uint256 _index) external view returns (uint256);
 
-    /// @notice Enumerate NFTs assigned to an owner
-    /// @dev Throws if `_index` >= `balanceOf(_owner)` or if
-    ///  `_owner` is the zero address, representing invalid NFTs.
-    /// @param _owner An address where we are interested in NFTs owned by them
-    /// @param _index A counter less than `balanceOf(_owner)`
-    /// @return The token identifier for the `_index`th NFT assigned to `_owner`,
-    ///   (sort order not specified)
-    function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
+    // function tokenOfOwnerByIndex(address _owner, uint256 _index) external view returns (uint256);
 
     function _mint(address to, uint256 tokenId) internal override(ERC721) {
         super._mint(to, tokenId);
+        // 1. add tokens to the owner
+        // 2. add tokens to our totalsupply - to allTokens
+
+        _addTokensToTotalSupply(tokenId);
+    }
+
+    function _addTokensToTotalSupply(uint256 tokenId) private {
+        _allTokens.push(tokenId);
     }
 }
