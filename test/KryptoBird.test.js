b6d20603b84b5ca83fbdda7925f5ad1f763ac7f1
Joowon Kim <0122wndnjs@gmail.com>
2022-01-20 13:07:49 +0900

    Minting test

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -8,19 +8,23 @@ require('chai')
 .should()
 
 contract('KryptoBird', (accounts) => {
-    let contract 
+    
+    let contract
+    // before tells our tests to run this first before anything else
+    before(async () => {
+        contract = await KryptoBird.deployed()
+    })
+
     // testing container - describe
     describe('deployment', async () => {
         // test samples with writing it
         it('deploys successfully', async () => {
-            contract = await KryptoBird.deployed()
             const address = contract.address
             assert.notEqual(address, '')
             assert.notEqual(address, null)
             assert.notEqual(address, undefined)
             assert.notEqual(address, 0x0)
         })
-
         // 1. Test that the name mataches on our contract using the assert.equal function
         it('has a name', async () => {
             const name = await contract.name()
@@ -30,6 +34,21 @@ contract('KryptoBird', (accounts) => {
         it('has a symbol', async () => {
             const symbol = await contract.symbol()
             assert.equal(symbol, 'KBIRDZ')
+        })
+    })
+
+    describe('minting', async () => {
+        it('creates a new token', async () => {
+            const result = await contract.mint('https...1')
+            const totalSupply = await contract.totalSupply()
+
+            assert.equal(totalSupply, 1)
+            const event = result.logs[0].args
+            assert.equal(event._from, '0x0000000000000000000000000000000000000000', 'from is the contract')
+            assert.equal(event._to, accounts[0], 'to is msg.sender')
+
+            // Failure
+            await contract.mint('https...1').should.be.rejected;
         })
     })
 })
