c49a634a37589e6c448fb503b8bd665330cf089a
Joowon Kim <0122wndnjs@gmail.com>
2022-01-20 12:09:58 +0900

    adding chai test for deployment

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -1,4 +1,4 @@
-const assert = require('chai')
+const {assert} = require('chai')
 
 const KryptoBird = artifacts.require('./KryptoBird')
 
@@ -6,3 +6,19 @@ const KryptoBird = artifacts.require('./KryptoBird')
 require('chai')
 .use(require('chai-as-promised'))
 .should()
+
+contract('KryptoBird', (accounts) => {
+    let contract 
+    // testing container - describe
+    describe('deployment', async () => {
+        // test samples with writing it
+        it('deploys successfully', async () => {
+            contract = await KryptoBird.deployed()
+            const address = contract.address
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+            assert.notEqual(address, 0x0)
+        })
+    })
+})
