190ea23c5dacb1836d3028d321efcd2e18f742de
Joowon Kim <0122wndnjs@gmail.com>
2022-01-20 14:51:34 +0900

    adding index testing

--- test/KryptoBird.test.js
+++ test/KryptoBird.test.js
@@ -51,4 +51,23 @@ contract('KryptoBird', (accounts) => {
             await contract.mint('https...1').should.be.rejected;
         })
     })
+
+    describe('indexing', async () => {
+        it('lists KryptoBirdz', async () => {
+            // Mint three new tokens
+            await contract.mint('https...2')
+            await contract.mint('https...3')
+            await contract.mint('https...4')
+            const totalSupply = await contract.totalSupply()
+
+            // Loop through list and grab KBirdz from list
+            let result = []
+            let KryptoBird
+
+            for (i = 1; i <= totalSupply; i++) {
+                KryptoBird = await contract.kryptoBirdz(i-1)
+                result.push(KryptoBird)
+            }
+        })
+    })
 })
